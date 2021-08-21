012d82917683f7feefc8347f909259d907960dbe
Raisa Alejnadra Silva Plata <raisa2201silva@gmail.com>
2021-08-21 03:04:29 -0400

    Base Scene

---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float velocidad = 30f;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
+        {
+        	transform.position += Vector3.left * velocidad * Time.deltaTime;
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
+        {
+        	transform.position += Vector3.right * velocidad * Time.deltaTime;
+        }
+        
+       /* if(Input.GetKey(KeyCode.up) || Input.GetKey("d"))
+        {
+        	transform.position += Vector3.right * velocidad * Time.deltaTime;
+        }*/
+
+    }
+}
