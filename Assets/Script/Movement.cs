2bbcc60e71b47ff5e2e23a735a4d53e9f9573a81
Raisa Alejnadra Silva Plata <raisa2201silva@gmail.com>
2021-11-29 21:26:32 +0100

    Last modifications

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -4,24 +4,30 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public Animator controller;
     public float velocidad = 30f;
+    public float h;
+    public float v;
+
 
     void Update()
     {
         if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
         {
+            controller.SetFloat("run", 4f);
         	transform.position += Vector3.left * velocidad * Time.deltaTime;
+
         }
 
         if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
         {
+            controller.SetFloat("run", 4f);
         	transform.position += Vector3.right * velocidad * Time.deltaTime;
         }
         
-       /* if(Input.GetKey(KeyCode.up) || Input.GetKey("d"))
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKey("w"))
         {
-        	transform.position += Vector3.right * velocidad * Time.deltaTime;
-        }*/
-
+             controller.SetTrigger("jump");
+        }
     }
 }
---
+++ Assets/Script/Mover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+    public Transform yo;
+    public float h;
+    public float v;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+    	// Movmiento horizontal
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+        yo.position = new Vector2(transform.position.x + (h * speed), 0);
+    }
+}
